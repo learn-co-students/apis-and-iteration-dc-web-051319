6f0b3118f912346708ffde044b33a74aa12c8792
Robert Pedersen <rob@worldcrimeleague.org>
2019-05-14 17:23:18 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,9 +6,33 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  sw_character_films = []
+
+
+sw_films = response_hash["results"].find do |hash|
+    hash["name"].downcase == character_name.downcase
+  end
+
+
+    sw_films["films"].each do |film_url|
+      sw_response_string = RestClient.get(film_url)
+      sw_response_hash = JSON.parse(sw_response_string)
+      sw_character_films <<  sw_response_hash["title"]
+    end
+    sw_character_films
+
+  end
+
+
+
+# add another step to parse the results hash
+
+
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -16,10 +40,13 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
+
+
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each_with_index do |value , index|
+    puts "#{index + 1}. #{value}"
+  end
 end
 
 def show_character_movies(character)
@@ -27,6 +54,7 @@ def show_character_movies(character)
   print_movies(films)
 end
 
+#get_character_movies_from_api("Luke Skywalker")
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
