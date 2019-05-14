1edbb5b402ddc08eaf4099dd56c43111c720b6d4
keithburgie <krburgie@gmail.com>
2019-05-14 16:40:37 -0400

    Added continue? method and Darth Vader

--- bin/run.rb
+++ bin/run.rb
@@ -8,10 +8,37 @@ def main
     character = get_character_from_user
     film_list = show_character_movies(character)
     if film_list == false
-        puts "Incorrect character name"
+        puts "Name not found."
         main
     else
         film_list
+        if continue?
+          main
+        else
+puts "                       .-. "
+puts "                      |_:_| "
+puts "                     /(_Y_)\ "
+puts ".                   ( \/M\/ ) "
+puts " '.               _.'-/'-'\-'._ "
+puts "   ':           _/.--'[[[[]'--.\_ "
+puts "     ':        /_'  : |::| :  '.\ "
+puts "       ':     //   ./ |oUU| \.'  :\ "
+puts "         ':  _:'..' \_|___|_/ :   :| "
+puts "           ':.  .'  |_[___]_|  :.':\ "
+puts "            [::\ |  :  | |  :   ; : \ "
+puts "             '-'   \/'.| |.' \  .;.' | "
+puts "             |\_    \  '-'   :       | "
+puts "             |  \    \ .:    :   |   | "
+puts "             |   \    | '.   :    \  | "
+puts "             /       \   :. .;       | "
+puts "            /     |   |  :__/     :  \\ "
+puts "           |  |   |    \:   | \   |   || "
+puts "          /    \  : :  |:   /  |__|   /| "
+puts "      snd |     : : :_/_|  /'._\  '--|_\ "
+puts "          /___.-/_|-'   \  \ "
+puts "                         '-' "
+puts "                      Goodbye"
+        end
     end
 end
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,10 +1,24 @@
 def welcome
   # puts out a welcome message here!
-  puts "Hello. Welcome to Starwars searcher."
+  puts "Hello. Welcome to Star Wars searcher."
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a character name:"
   user_input = gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
+end
+
+def continue?
+  puts "Would you like to search again? ( Y / N )"
+  user_input = gets.chomp
+  user_input = user_input.downcase
+  if user_input == "y" || user_input == "yes"
+    return true
+  elsif user_input == "n" || user_input == "no"
+    return false
+  else
+    puts "Input not recognized."
+    continue?
+  end
 end
